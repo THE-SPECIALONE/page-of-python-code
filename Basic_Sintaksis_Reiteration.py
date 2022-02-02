9994382f05cb0aca8c1dc95f9b722476e7ebaf99
Hiddan Saputro <hiddan2002@gmail.com>
2022-02-02 14:55:52 +0700

    Reiteration

---
+++ Basic_Sintaksis_Reiteration.py
@@ -0,0 +1,13 @@
+"""
+Progam perulangan membaca buku
+"""
+jumlah_buku = 8
+print('Perintah Ibu, "Baca semua bukumu"')
+
+jumlah_buku_yang_dibaca = 0
+print(f'jumlah buku yang dibaca {jumlah_buku_yang_dibaca}')
+
+for jumlah_buku_yang_dibaca in range(1, jumlah_buku+1):
+    print(f"buku ke{jumlah_buku_yang_dibaca} sudah dibaca")
+
+print(f'jumlah buku yang dibaca {jumlah_buku_yang_dibaca}')
