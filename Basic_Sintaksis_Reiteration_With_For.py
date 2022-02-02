b1b13e81cf7d04836707397888feaf3b408efe38
Hiddan Saputro <hiddan2002@gmail.com>
2022-02-02 18:51:30 +0700

    Reiteration with While

--- Basic_Sintaksis_Reiteration_With_For.py
+++ Basic_Sintaksis_Reiteration_With_For.py
@@ -1,5 +1,5 @@
 """
-Progam perulangan membaca buku
+Progam perulangan membaca buku dengan For
 """
 jumlah_buku = 8
 print('Perintah Ibu, "Baca semua bukumu"')
@@ -7,8 +7,7 @@ print('Perintah Ibu, "Baca semua bukumu"')
 jumlah_buku_yang_dibaca = 0
 print(f'jumlah buku yang dibaca {jumlah_buku_yang_dibaca}')
 
-# Dengan For
-
+print("dengan For")
 for jumlah_buku_yang_dibaca in range(1, jumlah_buku+1):
     print(f"buku ke{jumlah_buku_yang_dibaca} sudah dibaca")
 
