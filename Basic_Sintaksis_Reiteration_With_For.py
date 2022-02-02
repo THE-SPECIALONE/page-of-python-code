a4c2ea7ea6bad290288dd8eb3af0bdbb5992e954
Hiddan Saputro <hiddan2002@gmail.com>
2022-02-02 18:29:20 +0700

    Reiteration with For

--- Basic_Sintaksis_Reiteration_With_For.py
+++ Basic_Sintaksis_Reiteration_With_For.py
@@ -7,6 +7,8 @@ print('Perintah Ibu, "Baca semua bukumu"')
 jumlah_buku_yang_dibaca = 0
 print(f'jumlah buku yang dibaca {jumlah_buku_yang_dibaca}')
 
+# Dengan For
+
 for jumlah_buku_yang_dibaca in range(1, jumlah_buku+1):
     print(f"buku ke{jumlah_buku_yang_dibaca} sudah dibaca")
 
