16d0a5ab76f15eec451f75b76ceea7a8624b2158
Hiddan Saputro <hiddan2002@gmail.com>
2022-02-02 10:26:03 +0700

    Coding by Python

---
+++ Flow.py
@@ -0,0 +1,14 @@
+"""
+Semua sintacsis dasar bahasa pemrogman terdiri dari:
+1. Skuensial : Langkah berurutan
+2. Percobaan : Langkah melompat jika kondisi terpenuhi
+3. Perulangan : Mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+print('ibu berkata "Tolong ke warung belikan satu botol susu dan jika ada telur,beli 6 butir telur"')
+print('Budi menjawab"Siap,Bu"')
+print("Budi menuju ke warung")
+print('"Permisi, apakah ada susu?"')
+print("susu ada. Budi menghitung uang, cukup")
+print('"ada telur? sekalian kalo ada bu, 6 butir."')
+print("ada.")
+print("Budi pun membeli satu botol susu dan 6 butir telur, kemudian Ia pulang.")
