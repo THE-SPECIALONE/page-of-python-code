14b644f582e0912c2d8a10fd0b4489b2826ddcf8
Hiddan Saputro <hiddan2002@gmail.com>
2022-02-02 18:53:09 +0700

    Reiteration with For

--- Basic_Sintaksis_Reiteration_With_For.py
+++ Basic_Sintaksis_Reiteration_With_For.py
@@ -12,3 +12,4 @@ for jumlah_buku_yang_dibaca in range(1, jumlah_buku+1):
     print(f"buku ke{jumlah_buku_yang_dibaca} sudah dibaca")
 
 print(f'jumlah buku yang dibaca {jumlah_buku_yang_dibaca}')
+
