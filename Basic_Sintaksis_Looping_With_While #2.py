cfc67999cf11bb84334d0677d2aaaf8c49154aa7
Hiddan Saputro <hiddan2002@gmail.com>
2022-02-03 07:12:56 +0700

    Looping with For & While

--- Basic_Sintaksis_Looping_With_While #2.py
+++ Basic_Sintaksis_Looping_With_While #2.py
@@ -1,16 +1,23 @@
 """
 Progam perulangan membaca buku dengan While
 """
-jumlah_buku = 10
+buku = 10
 print('Perintah Ibu,"baca semua bukumu"')
+jumlah_baca = 0
 
-jumlah_buku_yang_sudah_dibaca_dan_sudah_dipahami = 0
-print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_sudah_dipahami}')
+buku_yang_sudah_dibaca_dan_dipahami = 0
+print(f'Jumlah buku yang sudah dibaca dan dipahami {buku_yang_sudah_dibaca_dan_dipahami}')
 
-while jumlah_buku_yang_sudah_dibaca_dan_sudah_dipahami < jumlah_buku:
-    jumlah_buku_yang_sudah_dibaca_dan_sudah_dipahami = jumlah_buku_yang_sudah_dibaca_dan_sudah_dipahami + 1
-    if jumlah_buku_yang_sudah_dibaca_dan_sudah_dipahami == 10:
-        print(f"buku ke {jumlah_buku_yang_sudah_dibaca_dan_sudah_dipahami} belum paham")
-    print(f"buku ke {jumlah_buku_yang_sudah_dibaca_dan_sudah_dipahami} sudah dibaca dan dipahami")
-
-print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_sudah_dipahami}')
+while jumlah_baca < buku * 2:
+    jumlah_baca = jumlah_baca + 1
+    if buku_yang_sudah_dibaca_dan_dipahami == 9:
+        print(f"buku ke {buku_yang_sudah_dibaca_dan_dipahami + 1} belum paham")
+    else:
+        buku_yang_sudah_dibaca_dan_dipahami = buku_yang_sudah_dibaca_dan_dipahami + 1
+        print(f"buku ke {buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami")
+    print(f'jumlah buku yang sudah dibaca dan dipahami {buku_yang_sudah_dibaca_dan_dipahami}')
+    if buku_yang_sudah_dibaca_dan_dipahami == buku:
+        print('"Bu, semua buku sudah dibaca dan dipahami"')
+else:
+    print(f'"Bu, tidak semua buku bisa dipahami. '
+          f'Budi hanya bisa memahami {buku_yang_sudah_dibaca_dan_dipahami} buku"')
